f51e127bef7e765bbfc486444ee8a059fb37d8ca
Maulen0806 <ma_nurzhanuly@kbtu.kz>
2026-02-05 19:29:51 +0600

    Update while_loop.py

--- loops/while_loop.py
+++ loops/while_loop.py
@@ -1,1 +1,8 @@
-
+floor = 1
+target = int(input())
+print(f"Elevator is now on {floor} floor")
+while floor != target:
+    direction = 1 if target > floor else -1
+    floor += direction
+    print(f"{floor}" if direction > 0 else f"{floor}")
+print("You've reached youre floor")
