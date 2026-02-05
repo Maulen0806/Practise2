5df844f03f6ae6b87a40e039209794a9129521b2
Maulen0806 <ma_nurzhanuly@kbtu.kz>
2026-02-05 17:06:39 +0600

    Update boolean_operators.py

--- boolean/boolean_operators.py
+++ boolean/boolean_operators.py
@@ -1,1 +1,5 @@
-
+is_true = True
+is_false = False
+print(f"The function and requires, 2 conditions to be true: True and True -> {is_true and is_true}")
+print(f"The function or requires, one of the conditions to be true: True or False -> {is_true or is_false}")
+print(f"The function not make booleans opposite: not True -> {not is_true}")
